1e2c2c31cfe7c93256de18e4d45925a3d842373a
José Carlos <zecarlosnetodantas@gmail.com>
2025-06-23 13:33:57 -0300

    [Feat]: Adicionando navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Navbar from './Navbar/Navbar'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+        <div className="bg-white h-screen flex justify-center items-center w-full">
+          <Navbar/>
+        </div>
     </>
   )
 }
