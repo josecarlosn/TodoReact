db086d332b0b68a229cd08209022281c3d88c373
José <zecarlosnetodantas@gmail.com>
2025-07-04 16:13:55 -0300

    [Feat]: Delete Modal adicionado com funcionalidade

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { useEffect } from 'react'
 import './App.css'
 import Container from './components/Container/Container'
 import Navbar from './components/Navbar/Navbar'
-import { TaskProvider } from './Context/TaskContext'
 import EditModal from './components/EditModal/EditModal'
-import { EditTaskProvider, useEditContext } from './Context/EditContext'
 import DeleteModal from './components/DeleteModal/DeleteModal'
 
 
 
 function App() {
-
-  const {editVisibility} = useEditContext()
   return (
-
     <div className="bg-white h-screen flex justify-center items-center w-full">
       <Navbar/>
       <Container/>
